29fa83bb98f08ca697f31f993072814b65ce1faa
Natália Ferreira <108440727+natferrx@users.noreply.github.com>
2023-10-16 10:33:27 -0300

    Add files via upload

---
+++ Desafio2_RelatorioVendas/AtalhoRelatórioPBIX.py
@@ -0,0 +1,4 @@
+
+# Atalho Ao Relatorio PBIX: https://app.powerbi.com/view?r=eyJrIjoiYmY5NjBjZDUtNDFjOC00MDQwLWEzZDktMTQ4NDJlNzc3ZTUyIiwidCI6ImFmMDYxM2IwLWI0YWEtNGYzNC05ZjNjLWNhMDQ2M2JiMGYwZiJ9
+
+# Para mais detalhes, favor ler o Docx completo :)
